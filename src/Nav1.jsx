ba8c4591739f5255afdb32f7651464b0b961cbb1
svharisaravanan <sharisaravanan@gmail.com>
2025-04-06 18:56:37 +0530

    usestate

--- src/Nav1.jsx
+++ src/Nav1.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import img1 from "./assets/img1.jpg";
 function Nav1(props) {
- function eve(){
-    console.log("click");
- }
+
+   const [Purchase, setPurchase] =useState(false);
+
+
+    function eve(){
+        setPurchase(true);  
+        console.log("purchased");
+    }
+
     return (
        
           <div className=" card">
             <img src={img1} alt="" />
             <p>name: {props.name}</p>   
             <p>Price:{props.name}</p>          
-            <button onClick={()=>{console.log("click")}}>click</button>               
+            <button onClick={eve}>Buy</button>    
+            
+            <p>{Purchase ? "Already purchased": "Buy now"}</p>         
         </div>
         
-    );}
-
-Nav1.defaultProps={
-    name:"haridefa",
+    );
 }
+
+        Nav1.defaultProps={
+            name:"haridefa",
+        }
 export default Nav1;
 
 
